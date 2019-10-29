3c0b362f2bfe01a1a5245a51b5479b34803b73c0
nboehl <56567664+nboehl@users.noreply.github.com>
2019-10-28 21:26:10 -0400

    Create data.ino
    Preliminary coordinate tracking code, must be modified.

---
+++ data.ino
@@ -0,0 +1,36 @@
+/*Preliminary code for tracking coordinates; MUST BE MODIFIED*/
+
+#include "Enes100.h"
+
+void setup() {
+    // Initialize Enes100 Library
+    // Team Name, Mission Type, Marker ID, TX Pin, RX Pin
+    Enes100.begin("DATA TEAM", DATA, 3, 8, 9);
+
+    Enes100.print("Destination is at (");
+    Enes100.print(Enes100.destination.x);
+    Enes100.print(", ");
+    Enes100.print(Enes100.destination.y);
+    Enes100.println(")");
+
+    // Any other setup code...
+}
+
+void loop() {
+    // Update the OSV's current location
+    if (Enes100.updateLocation()) {
+        Enes100.print("OSV is at (");
+        Enes100.print(Enes100.location.x);
+        Enes100.print(", ");
+        Enes100.print(Enes100.location.y);
+        Enes100.print(", ");
+        Enes100.print(Enes100.location.theta);
+        Enes100.println(")");
+    } else {
+        // OSV's location was not found
+        Enes100.println("404 Not Found");
+    }
+
+    // Transmit the message from the arduino
+    Enes100.mission('M');
+}
