a9e76f3d109152f72678c35e5058d4a9cde5623a
KidusYared <56655529+KidusYared@users.noreply.github.com>
2019-11-11 20:09:23 -0500

    Update Payload_Extraction_Code.ino

--- Payload_Extraction_Code.ino
+++ Payload_Extraction_Code.ino
@@ -12,7 +12,7 @@ void loop() {
   /* array elements numbered from 0 to array size - 1
    * Loop below repeated reads data from payload to create array by
    * reading voltage from payload pads */
-  for (int i = 0; i < 15; i++) { //16 - 1 since array starts at 0
+  for (int i = 0; i < 16; i++) { //16 - 1 since array starts at 0
     int analogValue = analogRead(A1); //read input and find voltage
     inputVoltage = (analogValue*5.0)/1024.0;
       //if signal reads 0V, 0 entered in array
@@ -28,7 +28,7 @@ void loop() {
   /* Takes whole received array and leaves only last 7 bits
    * Ignores first 9 bits which are the signal code
    * the print char */
-  for (int i = 0; i < 6; i++){ //7 - 1 since array starts at 0
+  for (int i = 0; i < 7; i++){ //7 - 1 since array starts at 0
     charArray[i] = payloadArray[(i+9)];
   }
   Serial.print("Data Received: ");
